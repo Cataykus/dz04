6e4a65ea4eeb1abb62486e9f8d34bff315608850
Cataykus <cataykus@gmail.com>
2020-05-11 13:39:57 +0300

    q

---
+++ Unitydz04/Assets/Scripts/MobSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] _mobSpawners;
+    [SerializeField] private GameObject _enemyPrefab;
+
+    void Start()
+    {
+        StartCoroutine(SpawnMobs());
+    }
+
+    private IEnumerator SpawnMobs()
+    {
+        while (true)
+        {
+            foreach (var spawner in _mobSpawners)
+            {
+                Instantiate(_enemyPrefab, spawner.position, Quaternion.identity);
+
+                yield return new WaitForSeconds(2);
+            }
+        }
+    }
+}
